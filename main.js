1a38c8cd4507037347edb6977a6db2eb8cbf5960
chaitanya1612 <73599136+chaitanya1612@users.noreply.github.com>
2021-06-18 22:05:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function take_snapshot() {
     });
 }
 console.log('ml5 version:',ml5.version);
-classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/fRc7RXIO0/model.json',modelLoaded);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Ld6twZJea/model.json',modelLoaded);
 function modelLoaded()
 {
 console.log('model loaded');
@@ -29,4 +29,40 @@ var synth = window.speechSynthesis;
  speak_data_2="and the second prediction is "+prediction_2;
 var utterThis= new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
 synth.speak(utterThis);
+}
+function check(){
+    img=document.getElementById('captured_image');
+    classifier.classify(img,gotResult);
+}
+
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML=results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML=results[1].label;
+        prediction_1=results[0].label;
+        prediction_2=results[1].label;
+        speak();
+        if(results[0].label=="That was a sweet victory"){
+            document.getElementById("update_emoji").innerHTML="&#9996";
+        }
+        if(results[0].label=="you are the best"){
+            document.getElementById("update_emoji").innerHTML="&#128077"; 
+        }
+        if(results[0].label=="That was amazing"){
+            document.getElementById("update_emoji").innerHTML="&#128076";
+        }
+        if(results[1].label=="That was a sweet victory"){
+            document.getElementById("update_emoji2").innerHTML="&#9996";     
+        }
+        if(results[1].label=="you are the best"){
+            document.getElementById("update_emoji2").innerHTML="&#128077"; 
+        }
+        if(results[1].label=="That was amazing"){
+            document.getElementById("update_emoji2").innerHTML="&#128076";
+        }
+    }
 }
